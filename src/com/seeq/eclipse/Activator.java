7094f8626916c78dc57786d7a1332eec6fa8d1b4
Paul Verest <paul.verest@live.com>
2014-12-16 13:35:33 +0800

    Log to workspace log (see ErrorLog View)

--- src/com/seeq/eclipse/Activator.java
+++ src/com/seeq/eclipse/Activator.java
@@ -1,9 +1,16 @@
 package com.seeq.eclipse;
 
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.*;
 
-public class Activator implements BundleActivator {
+public class Activator extends Plugin {
+	
+    // The plug-in ID
+    public static final String PLUGIN_ID = "com.seeq.eclipse.importprojects"; //$NON-NLS-1$
 
+    // The shared instance
+    private static Activator plugin;
+	
 	private static BundleContext context;
 
 	public static BundleContext getContext() {
@@ -16,6 +23,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		plugin = this;
 	}
 
 	/*
@@ -24,6 +32,16 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+		plugin = null;
 	}
 
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+	
 }
